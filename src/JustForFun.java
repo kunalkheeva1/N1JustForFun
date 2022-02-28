335a84980a209a60f325f2194de4368f39207009
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-02-27 21:39:05 -0800

    kkk

--- src/JustForFun.java
+++ src/JustForFun.java
@@ -1,31 +1,45 @@
-
-
 public class JustForFun {
+  void leftRotate(int arr[],int d, int n){
+    d=d%n;
+    int i, j, k, temp;
+    int g_c_d = gcd(d,n);
+    for( i =0; i<g_c_d; i++){
+      temp= arr[i];
+      j=i;
+      while(true) {
+        k = j + d;
+        if (k >= n)
+          k -= n;
+        if (k == i)
+          break;
+        arr[j] = arr[k];
+        j = k;
+      }
+         arr[j]=temp ;
+      }
+    }
+    void printArray(int arr[], int size){
+      int i;
+      for(i=0; i<size; i++)
+        System.out.println(arr[i]+" ");
 
-  void leftRotate(int arr[], int d, int n){
-    for(int i = 0; i<d; i++)
-      leftRotateByOne(arr,n);
 
-  }void leftRotateByOne(int arr[], int n){
-    int i, temp;
-    temp= arr[0];
-    for( i=0; i<n-1; i++)
-      arr[i]=arr[i+1];
-   arr[n-1]= temp;
+    } int gcd (int a , int b){
+      if(b==0)
+        return a;
+      else
+        return gcd(b, a%b);
 
-  }
-  void printArray(int arr[], int n){
-    for(int i=0; i<n; i++ )
-      System.out.println(arr[i]+ " " );
-  }
-  public static void main(String[]args){
-    JustForFun rotate= new JustForFun();
-    int [] arr= {1,2,3,4,5,6,7};
-    rotate.leftRotate(arr, 4, 7);
-  rotate.printArray(arr,7);
-  }
+
+    }
+    public static void main(String []args ){
+    JustForFun rotate = new JustForFun();
+    int arr[]= {1,2,3,4,5,6,7,8,9,10};
+    rotate.leftRotate(arr,2,10);
+    rotate.printArray(arr,10);
 
 
 
 
     }
+  }
