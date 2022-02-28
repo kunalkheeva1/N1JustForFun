929fd84ae6a1a727f40e48cb87e3e39395d2c66d
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-02-27 20:32:42 -0800

    kkk

--- src/JustForFun.java
+++ src/JustForFun.java
@@ -2,18 +2,30 @@
 
 public class JustForFun {
 
-  public static int[] m1() {
+  void leftRotate(int arr[], int d, int n){
+    for(int i = 0; i<d; i++)
+      leftRotateByOne(arr,n);
+
+  }void leftRotateByOne(int arr[], int n){
+    int i, temp;
+    temp= arr[0];
+    for( i=0; i<n-1; i++)
+      arr[i]=arr[i+1];
+   arr[n-1]= temp;
+
+  }
+  void printArray(int arr[], int n){
+    for(int i=0; i<n; i++ )
+      System.out.println(arr[i]+ " " );
+  }
+  public static void main(String[]args){
+    JustForFun rotate= new JustForFun();
+    int [] arr= {1,2,3,4,5,6,7};
+    rotate.leftRotate(arr, 4, 7);
+  rotate.printArray(arr,7);
+  }
 
 
-      return new int[]{1, 3, 4, 5};}
-
-
-    public static void main (String[]args){
-      int[] kk = m1();
-      for(int i=0; i<kk.length; i++){
-        System.out.println(kk[i]);
 
 
     }
-  }
-}
